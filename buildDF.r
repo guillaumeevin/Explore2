08c8adbed061c6bbfbdb724b763c5ce6ee6f27da
guillaumeevin <eving@ad.u-ga.fr>
2025-03-31 17:35:59 +0200

    - coordMeteo.rds is a list of matrices indicating the coordinates of the 8981 pixels of the SAFRAN grid - runQUALYPSO produces QUALYPSO outputs for the meteo indicators

--- buildDF.r
+++ buildDF.r
@@ -1,4 +1,8 @@
-scen = data.frame(rbind(c("rcp26","CNRM-CM5-LR","ALADIN63"),
+# Guillaume Evin
+# Explore2
+# build data frame indicating the scenarios
+
+dfScenario = data.frame(rbind(c("rcp26","CNRM-CM5-LR","ALADIN63"),
                         c("rcp26","EC-EARTH","HadREM3-GA7-05"),
                         c("rcp26","EC-EARTH","RACMO22E"),
                         c("rcp26","EC-EARTH","RCA4"),
@@ -15,8 +19,8 @@ scen = data.frame(rbind(c("rcp26","CNRM-CM5-LR","ALADIN63"),
                         c("rcp45","IPSL-CM5A-MR","RCA4"),
                         c("rcp45","MPI-ESM-LR","CCLM4-8-17"),
                         c("rcp45","MPI-ESM-LR","REMO"),
-                        c("rcp45"," NorESM1-M","HIRHAM5"),
-                        c("rcp45"," NorESM1-M","REMO"),
+                        c("rcp45","NorESM1-M","HIRHAM5"),
+                        c("rcp45","NorESM1-M","REMO"),
                         c("rcp85","CNRM-CM5-LR","ALADIN63"),
                         c("rcp85","CNRM-CM5-LR","HadREM3-GA7-05"),
                         c("rcp85","EC-EARTH","HadREM3-GA7-05"),
@@ -30,7 +34,18 @@ scen = data.frame(rbind(c("rcp26","CNRM-CM5-LR","ALADIN63"),
                         c("rcp85","IPSL-CM5A-MR","RCA4"),
                         c("rcp85","MPI-ESM-LR","CCLM4-8-17"),
                         c("rcp85","MPI-ESM-LR","RegCM4-6"),
-                        c("rcp85","MPI-ESM-LR"," REMO"),
+                        c("rcp85","MPI-ESM-LR","REMO"),
                         c("rcp85","NorESM1-M","HIRHAM5"),
-                        c("rcp85","NorESM1-M"," REMO"),
+                        c("rcp85","NorESM1-M","REMO"),
                         c("rcp85","NorESM1-M","WRF381P")))
+colnames(dfScenario) = c("rcp","gcm","rcm")
+
+
+dfIndicMeteo = data.frame(rbind(c("tasAdjust","tas","DJF"),
+                                c("tasAdjust","tas","JJA"),
+                                c("prtotAdjust","pr","DJF"),
+                                c("prtotAdjust","pr","JJA"),
+                                c("evspsblpotAdjust","evspsblpot","JJA"),
+                                c("evspsblpotAdjust","evspsblpot","DJF"),
+                                c("RX1day","rx1day","year")))
+colnames(dfIndicMeteo) = c("folder","var","season")
--- runQUALYPSOmeteo.r
+++ runQUALYPSOmeteo.r
@@ -3,156 +3,120 @@
 
 library(ncdf4)
 library(CFtime)
+library(QUALYPSO)
 
-setwd("D:/PROJET/")
-path_data="D:/PROJET/Explore2/data/Rx1D/"
+# parallel computation and monitor progress
+library(doSNOW)
+cl <- makeSOCKcluster(30)
+registerDoSNOW(cl)
 
 ##########################################
-# read selection of scenarios & catchments #
+# read selection of scenarios and pixels #
 ##########################################
 
-# list runs
-scenAvail = readRDS("D:/PROJET/Explore2/PAPIER/QUALYPSO/QUALYPSOOUT/meteo_France/scenAvail.rds")
-dfPixel = readRDS("D:/PROJET/Explore2/PAPIER/QUALYPSO/QUALYPSOOUT/meteo_France/dfPixel.rds")
-
-
-
-#for(iindic in 1:nrow(dfIndic)){
-# choose one variable / one indic
-#var = dfIndic$var[iindic]
-#indic = dfIndic$indic[iindic]
-var = "rx1day"
-
-
+# dfIndicMeteo and dfScenario
+source("./buildDF.r")
 
 # Extract indices for all catchments/scenarios/years
 ref_year = 1990
 final_year = 2100
 vecY = ref_year:final_year
 nY = length(vecY)
-nS = nrow(scenAvail)
+nS = nrow(dfScenario)
 
-# indicateurs
-Y = array(dim=c(nbas,nS,nY))
+# coordinates France/SAFRAN grid: list of matrices
+lCoords = readRDS(file = "./coordMeteo.rds")
+nLon = nrow(lCoords$lon)
+nLat = ncol(lCoords$lon)
+isPixel = lCoords$mask
+indexPixel = which(isPixel==1,arr.ind = T)
+nPixels = sum(isPixel)
 
-for(is in 1:nS){
-  r = scenAvail[is,]
-  typeagg = strsplit(indic,"_")[[1]][1]
-  peragg = strsplit(indic,"_")[[1]][2]
-  file_data = list.files(paste0(path_data,var,"/"),full.names=T,
-                         pattern=glob2rx(paste0(var,"*",r$rcp,"*",r$gcm,"*",r$rcm,"*",r$bc,"*",typeagg,"*",peragg,"*")))
-  
-  file_data = paste0(path_data,"rx1day_year_ICHEC-EC-EARTH_KNMI-RACMO22E_ADAMONT_rcp26_")
-  
-  
-  
-  # get variables
-  ncin <- nc_open(file_data)
-  
-  # get and convert time
-  time <- ncvar_get(ncin,"time")
-  tunits <- ncatt_get(ncin,"time","units")
-  vecTimeCf <- CFtime(tunits$value, calendar = "proleptic_gregorian", time) # convert time to CFtime class
-  vecTimeDates <- as_timestamp(vecTimeCf, format = "timestamp")
-  
-  v = var.get.nc(nc,var)
-  time = var.get.nc(nc,"time")
-  att.get.nc(nc, "time","units")
-  cf <- CFtime(att.get.nc(nc, "time","units"), att.get.nc(nc, "time","calendar"), time)
-  vecyear <- as.numeric(format(as.Date(CFtimestamp(cf)),"%Y"))
-  close.nc(nc)
-  
-  # fill array
-  iY = match(vecY,vecyear)
-  Y[,is,] = v[,iY]
-}
+# path to netcdf files
+path_data = "../indicMeteo/"
 
-
-
-# prepare inputs for QUALYPSO
-if(var=="tasAdjust"){
-  typechangeVar="abs"
-  spar=1
-}else{
-  typechangeVar="rel"
-  spar=1.1
-}
-probCI = 0.9
-X=vecY
-Xfut=seq(from=1990,to=2100,by=5)
-
-################ LOOP OVER THE CATCHMENTS ################ 
-
-# create folders if needed
-fQ = paste0("C:/Explore2/QUALYPSO/",typeRes,"/")
-if (!file.exists(fQ))  dir.create(fQ)
-fQI = paste0("C:/Explore2/QUALYPSO/",typeRes,"/",var,"_",indic,"/")
-if (!file.exists(fQI))  dir.create(fQI)
-
-# call QUALYPSO
-for(ibv in 1:nbas){
-  # for one catchment
-  Ybv = Y[ibv,,]
+for(iindic in 1:nrow(dfIndicMeteo)){
+  rInd = dfIndicMeteo[iindic,]
   
-  ## climate response
-  CLIMATERESPONSE = prepare_clim_resp(Y = Ybv, X = X, Xfut = Xfut, typeChangeVariable=typechangeVar, spar=spar,
-                                      type="spline",scenAvail=scenAvail)
-  saveRDS(CLIMATERESPONSE,file=paste0(fQI,vecBassins[ibv],"_CR.rds"))
-  #plot(-1,-1,xlim=range(X),ylim=range(clim_resp$phiStar))
-  #for(i in 1:nrow(clim_resp$phiStar)) lines(Xfut,clim_resp$phiStar[i,],col=i)
+  # choose one variable / one indic
+  varlong = rInd$folder
+  var = rInd$var
+  season = rInd$season
   
-  # list of options
-  listOption = list(spar=spar,typeChangeVariable=typechangeVar,probCI=probCI,climResponse=CLIMATERESPONSE)
+  # indicateurs
+  Y = array(dim=c(nLon,nLat,nS,nY))
   
-  # call QUALYPSO
-  QOUT = QUALYPSO(Y=Ybv, #one Y and run per pixel because otherwise we cannot have several future times
-                  scenAvail=scenAvail,
-                  X=X,
-                  Xfut=Xfut,
-                  listOption=listOption)
-  lS = QOUT$listScenarioInput
-  
-  # reconstructed climate responses and replace by available values
-  phiReconst=reconstruct_chains(QOUT)
-  phiStar = CLIMATERESPONSE$phiStar
-  
-  #########  Missing scenarios   #########
-  vScenComp <- apply(lS$scenComp, 1, paste, collapse='.')
-  vScenAvail <- apply(lS$scenAvail, 1, paste, collapse='.')
-  iMatchScen = match(vScenAvail,vScenComp)
-  phiReconst[iMatchScen,]=phiStar
-  
-  # add estimates of quantiles from the reconstructed ensemble
-  ieff_rcp = which(colnames(scenAvail)=="rcp")
-  for(rcp in unique(scenAvail$rcp)){
-    ieff_this_rcp=which(lS$listEff[[ieff_rcp]]==rcp)
-    chg_mean = QOUT$MAINEFFECT$rcp$MEAN[,ieff_this_rcp] + QOUT$GRANDMEAN$MEAN
+  for(is in 1:nS){
+    rSc = dfScenario[is,]
+    file_data = list.files(paste0(path_data,varlong,"/"),full.names=T,
+                           pattern=glob2rx(paste0(var,"*",rSc$rcp,"*",rSc$gcm,"*",rSc$rcm,"*ADAMONT*",season,"*")))
     
-    # compute a multiplicative factor SDtot/SD(phi*) to match the standard deviation for each RCP
-    # obtained from QUALYPSO
-    sd.qua = sqrt(QOUT$TOTALVAR-QOUT$INTERNALVAR-QOUT$EFFECTVAR[,ieff_rcp])
-    if(any(is.na(sd.qua))){
-      chg_mean[which(is.na(sd.qua))]=NA
-    }
+    # get variables
+    nc <- nc_open(file_data)
+    v = ncvar_get(nc,varlong)
+    time = ncvar_get(nc,"time")
+    time <- ncvar_get(nc,"time")
+    tunits <- ncatt_get(nc,"time","units")
+    vecTimeCf <- CFtime(tunits$value, calendar = "proleptic_gregorian", time) # convert time to CFtime class
+    vecTimeDates <- as_timestamp(vecTimeCf, format = "timestamp")
+    vecyear <- as.numeric(format(as.Date(vecTimeDates),"%Y"))
+    nc_close(nc)
     
-    # reconcstructed phiStar for this rcp
-    idx_this_rcp=which(lS$scenComp[,ieff_rcp]==rcp)
-    phiRCP = phiReconst[idx_this_rcp,]
-    sd.emp = apply(phiRCP,2,sd)
-    sd.emp[sd.emp==0] = 1 # avoid NaN for the reference year
-    sd.corr = sd.qua/sd.emp
-    phiStar.corr = phiRCP*t(replicate(dim(phiRCP)[1],sd.corr))
-    chg_q5 = apply(phiStar.corr,2,quantile,probs = (1-probCI)/2,na.rm=T)
-    chg_q95 = apply(phiStar.corr,2,quantile,probs = 0.5+probCI/2,na.rm=T)
-    
-    QOUT[[rcp]] = rbind(chg_q5,chg_mean,chg_q95)
+    # fill array
+    iY = match(vecY,vecyear)
+    Y[,,is,] = v[,,iY]
   }
   
   
-  QOUT$listOption=NA #to not store twice the same information
-  QOUT$RESERR=NA
-  QOUT$CHANGEBYEFFECT=NA
-  QOUT$CLIMATERESPONSE=NA
-  saveRDS(QOUT,file=paste0(fQI,vecBassins[ibv],"_Q.rds"))
+  
+  # prepare inputs for QUALYPSO
+  if(var=="tas"){
+    typechangeVar="abs"
+    spar=1
+  }else{
+    typechangeVar="rel"
+    spar=1.1
+  }
+  probCI = 0.9
+  X=vecY
+  Xfut=seq(from=1990,to=2100,by=5)
+  
+  ################ LOOP OVER THE PIXELS ################ 
+  
+  # create folders if needed
+  fQ = paste0("../QUALYPSOOUT/Meteo/")
+  if (!file.exists(fQ))  dir.create(fQ)
+  fQI = paste0(fQ,var,"_",season,"/")
+  if (!file.exists(fQI))  dir.create(fQI)
+  
+  # call QUALYPSO
+  # monitor progress
+  pb <- txtProgressBar(min=1, max=nLon, style=3)
+  progress <- function(n) setTxtProgressBar(pb, n)
+  opts <- list(progress=progress)
+  
+  foreach(iPixel=1:nPixels, .packages = c('QUALYPSO'), .options.snow=opts) %dopar% {
+    # for one pixel
+    i1 = indexPixel[iPixel,1]
+    i2 = indexPixel[iPixel,2]
+    
+    Yp = Y[i1,i2,,]
+    
+    
+    # list of options
+    listOption = list(spar=spar,typeChangeVariable=typechangeVar,probCI=probCI)
+    
+    # call QUALYPSO
+    QOUT = QUALYPSO(Y=Yp, #one Y and run per pixel because otherwise we cannot have several future times
+                    scenAvail=dfScenario,
+                    X=vecY,
+                    Xfut=Xfut,
+                    listOption=listOption)
+    
+    QOUT$listOption=NA #to not store twice the same information
+    QOUT$RESERR=NA
+    QOUT$CHANGEBYEFFECT=NA
+    QOUT$CLIMATERESPONSE=NA
+    saveRDS(QOUT,file=paste0(fQI,i1,"_",i2,".rds"))
+  }
 }
-#}
